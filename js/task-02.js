79530102f6ba5b7f7c3c43c11c36f961ff7dfccf
ksholovko <132094880+ksholovko@users.noreply.github.com>
2023-08-16 22:47:07 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const createIngredients = ingredients.map((ingredient, index) => {
+  const createIngredient = document.createElement('li');
+  createIngredient.classList.add('item');
+  createIngredient.textContent = ingredients[index];
+ 
+  return createIngredient;
+});
+
+console.log(createIngredients);
+
+ingredientsList.append(...createIngredients);
