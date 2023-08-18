f7fad784bedce1b083bc63cd885dfc8ae198f3dd
ksholovko <132094880+ksholovko@users.noreply.github.com>
2023-08-19 00:18:53 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+
+const changeColorBtn = document.querySelector('button.change-color');
+const colorHex = document.querySelector('.color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', getBackgroundColor);
+
+function getBackgroundColor() {
+
+  body.style.backgroundColor = getRandomHexColor();
+  colorHex.textContent = getRandomHexColor();
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
