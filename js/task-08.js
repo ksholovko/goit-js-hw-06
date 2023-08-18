fe53ca9811f960f05bdaa95a51cba7607a9aaa76
ksholovko <132094880+ksholovko@users.noreply.github.com>
2023-08-18 23:53:46 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    if (formElements.email.value === '' || formElements.password.value === '') {
+        alert('Fill out all the required input fields!');
+        return
+    
+    }
+        const formData = {
+            email: formElements.email.value,
+            password: formElements.password.value,
+    };
+    
+    console.log(formData);
+    
+    event.currentTarget.reset();
+    
+}
+
